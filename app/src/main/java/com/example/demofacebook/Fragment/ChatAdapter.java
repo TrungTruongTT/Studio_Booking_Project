0ef3fef6a6c67164ee979b84722411ee37e9317d
TRUONGTRUNG <trungttse151131@fpt.edu.vn>
2023-06-09 07:10:54 +0700

    chat 1

---
+++ app/src/main/java/com/example/demofacebook/Fragment/ChatAdapter.java
@@ -0,0 +1,53 @@
+package com.example.demofacebook.Fragment;
+
+import androidx.annotation.NonNull;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.demofacebook.Model.Message;
+import com.example.demofacebook.R;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+private List<Message> sListMessage;
+public void setDate(List<Message> list){
+this.sListMessage = list;
+notifyDataSetChanged();
+}
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+    return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        Message message = sListMessage.get(position);
+        //9:46
+    }
+
+    @Override
+    public int getItemCount() {
+    if(sListMessage !=null){
+        return sListMessage.size();
+    }
+        return 0;
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvMessage;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvMessage = itemView.findViewById(R.id.tv_message);
+        }
+    }
+}
--- app/src/main/java/com/example/demofacebook/Fragment/ChatFragment.java
+++ app/src/main/java/com/example/demofacebook/Fragment/ChatFragment.java
@@ -4,19 +4,42 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.demofacebook.R;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment{
+
+
+    private RecyclerView rcvMessage;
+    private EditText editMessage;
+    private Button btnSend;
+    /*private MessageAdapter messageAdapter;*/
+    private List<Message> sListMessage;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        editMessage.findViewById(R.id.edit_message);
+        btnSend.findViewById(R.id.btn_send);
+        rcvMessage.findViewById(R.id.rcv_message);
+
+
+        sListMessage= new ArrayList<>();
+
 
     }
 
