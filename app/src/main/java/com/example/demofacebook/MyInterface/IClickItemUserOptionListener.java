d9e9130fafaba2ed89f5138045c18e36424dfa49
pqt01 <thinhpqse151077@fpt.edu.vn>
2023-06-16 13:14:20 +0700

    add order, favorite

--- app/src/main/java/com/example/demofacebook/MyInterface/IClickItemUserOptionListener.java
+++ app/src/main/java/com/example/demofacebook/MyInterface/IClickItemUserOptionListener.java
@@ -1,6 +1,6 @@
 package com.example.demofacebook.MyInterface;
 
-import com.example.demofacebook.Model.Category;
+//import com.example.demofacebook.Model.Category;
 
 public interface IClickItemUserOptionListener {
     void onClickItemUserOptionListener(String option);
