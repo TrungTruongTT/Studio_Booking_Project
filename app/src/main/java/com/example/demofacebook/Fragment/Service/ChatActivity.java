188822511b53f1764572e038488e29190b77e035
TRUONGTRUNG <trungttse151131@fpt.edu.vn>
2023-06-18 18:52:13 +0700

    Add libs MOMO + VNPAY + ZALOPAY

---
+++ app/src/main/java/com/example/demofacebook/Fragment/Service/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.demofacebook.Fragment.Service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demofacebook.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
